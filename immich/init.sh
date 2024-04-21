060359f54bd13e9b23dca70291156fe444e13e63
Paolo Gentili <gentilipaolo92@gmail.com>
2024-04-21 19:59:30 +0000

    Change: back to docker volume for immich postgre

--- immich/init.sh
+++ immich/init.sh
@@ -2,6 +2,7 @@
 
 DIRNAME=$(dirname "$(realpath "$0")")
 COMPOSE=$DIRNAME/docker-compose.yml
+source $DIRNAME/.env
 
 docker compose -f $COMPOSE down -v
 docker compose -f $COMPOSE pull
@@ -12,7 +13,7 @@ DUMP="$DIRNAME/dump.sql.gz"
 if [[ -e $DUMP ]]; then
     echo "Restoring Immich DB backup..."
     sleep 10    # Wait for Postgres server to start up
-    gunzip < $DUMP | docker exec -i immich_postgres psql -U postgres -d immich    # Restore Backup
+    gunzip < $DUMP | docker exec -i immich_postgres psql -U $DB_USERNAME -d $DB_DATABASE_NAME    # Restore Backup
 fi
 
 docker compose -f $COMPOSE up -d
