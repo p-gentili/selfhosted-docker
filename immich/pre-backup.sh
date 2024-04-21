83c1c0ac54589b5ec7c4ee9b27c7f9ff85cb25ab
Paolo Gentili <gentilipaolo92@gmail.com>
2024-04-21 19:59:30 +0000

    Change: back to docker volume for immich postgre

--- immich/pre-backup.sh
+++ immich/pre-backup.sh
@@ -2,5 +2,6 @@
 
 set -e 
 DIRNAME=$(dirname "$(realpath "$0")")
+source $DIRNAME/.env
 
-docker exec -t immich_postgres pg_dumpall -c -U postgres | gzip > "$DIRNAME/dump.sql.gz"
+docker exec -t immich_postgres pg_dumpall -c -U $DB_USERNAME | gzip > "$DIRNAME/dump.sql.gz"
