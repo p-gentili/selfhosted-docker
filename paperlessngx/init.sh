95fa3e712167dbcbc1057dec533ed852b2850518
Paolo Gentili <gentilipaolo92@gmail.com>
2025-06-19 07:24:09 +0000

    Add: paperless ngx

---
+++ paperlessngx/init.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+DIRNAME=$(dirname "$(realpath "$0")")
+COMPOSE=$DIRNAME/docker-compose.yml
+source $DIRNAME/.env
+
+docker compose -f $COMPOSE down -v
+docker compose -f $COMPOSE pull
+docker compose -f $COMPOSE create
+docker start immich_postgres
+
+DUMP="$DIRNAME/dump.sql.gz"
+if [[ -e $DUMP ]]; then
+    echo "Restoring paperless-ngx DB backup..."
+    sleep 10    # Wait for Postgres server to start up
+    gunzip < $DUMP | docker exec -i immich_postgres psql -U paperless -d paperless    # Restore Backup
+fi
+
+docker compose -f $COMPOSE up -d
