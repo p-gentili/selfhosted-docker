dcb3871882417ed2c63dad4ab743a5d8bb307cba
Paolo Gentili <gentilipaolo92@gmail.com>
2025-08-01 09:35:37 +0000

    change: updated immich version and docker compose

--- immich/init.sh
+++ immich/init.sh
@@ -5,6 +5,7 @@ COMPOSE=$DIRNAME/docker-compose.yml
 source $DIRNAME/.env
 
 docker compose -f $COMPOSE down -v
+rm -rf $DB_DATA_LOCATION
 docker compose -f $COMPOSE pull
 docker compose -f $COMPOSE create
 docker start immich_postgres
@@ -13,7 +14,9 @@ DUMP="$DIRNAME/dump.sql.gz"
 if [[ -e $DUMP ]]; then
     echo "Restoring Immich DB backup..."
     sleep 10    # Wait for Postgres server to start up
-    gunzip < $DUMP | docker exec -i immich_postgres psql -U $DB_USERNAME -d $DB_DATABASE_NAME    # Restore Backup
+    gunzip --stdout $DUMP \
+	    | sed "s/SELECT pg_catalog.set_config('search_path', '', false);/SELECT pg_catalog.set_config('search_path', 'public, pg_catalog', true);/g" \
+	    | docker exec -i immich_postgres psql --dbname=$DB_DATABASE_NAME --username=$DB_USERNAME  # Restore Backup
 fi
 
 docker compose -f $COMPOSE up -d
